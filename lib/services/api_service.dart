c3c46031aa5e0fa592b5d3e0cbb37223a90e3dd9
unknown <clous.namsoo@hotmail.com>
2023-01-15 22:10:16 +0900

    fetch data to state

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,16 +1,22 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:toonflix/model/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodayToons() async {
+  Future<List<WebtoonModel>> getTodayToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     Uri url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
-
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final List<dynamic> data = jsonDecode(response.body);
+      for (var webtoon in data) {
+        webtoonInstances.add(WebtoonModel.fromjson(webtoon));
+      }
+      return webtoonInstances;
     }
 
     throw Error();
