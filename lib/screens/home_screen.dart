a6255429472696a31af71135d06abe0c4665fe47
unknown <clous.namsoo@hotmail.com>
2023-01-16 14:53:31 +0900

    create webtoon card

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -24,7 +24,14 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return const Text('123');
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                Expanded(child: makeList(snapshot)),
+              ],
+            );
           }
           if (snapshot.hasError) {
             return const Text('Error');
@@ -32,6 +39,47 @@ class HomeScreen extends StatelessWidget {
           return const Text('Loading..');
         },
       ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      padding: const EdgeInsets.all(20),
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              width: 200,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black.withOpacity(0.5),
+                      blurRadius: 15,
+                      offset: const Offset(10, 10))
+                ],
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Image.network(webtoon.thumb),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(fontSize: 22),
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) {
+        return const SizedBox(
+          width: 20,
+        );
+      },
+      itemCount: snapshot.data!.length,
     );
   }
 }
